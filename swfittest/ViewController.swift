ffc4aa7040303bf032695e9c59fe74e2686c000f
robert.qiu <qwjcool@gmail.com>
2015-01-09 13:22:49 +0800

    Initial Commit

---
+++ swfittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swfittest
+//
+//  Created by Robert Qiu on 1/9/15.
+//  Copyright (c) 2015 Robert Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
